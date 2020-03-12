13acb1c4ceb217868f4c564dce94a5ceb8bee97a
guillemtubert <guillemtubertsesen@gmail.com>
2020-03-12 09:49:31 +0100

    chat bug fix changed from localhost

---
+++ public/precache-manifest.4574db72e7e10d3b6dbc515295b4bba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee23a2081897915ac0a707c039f3de6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9d4bbb69da535bb3f6b",
+    "url": "/static/css/main.b0246b7a.chunk.css"
+  },
+  {
+    "revision": "90b5d9659db4687de110",
+    "url": "/static/js/2.3abd5d70.chunk.js"
+  },
+  {
+    "revision": "e1345d47b220a41451b8c23eb83bc290",
+    "url": "/static/js/2.3abd5d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d4bbb69da535bb3f6b",
+    "url": "/static/js/main.a604bfda.chunk.js"
+  },
+  {
+    "revision": "5dde3ecc687698d22105",
+    "url": "/static/js/runtime-main.22b866d5.js"
+  }
+]);
